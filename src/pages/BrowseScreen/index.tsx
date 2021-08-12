19fac0f75926f48f86bc68f0e1678dbfee94fbbe
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-08-12 14:59:03 +0700

    chore(BrowseScreen): to do

--- src/pages/BrowseScreen/index.tsx
+++ src/pages/BrowseScreen/index.tsx
@@ -3,7 +3,6 @@ import { useLocation } from "react-router";
 import AnimeCard from "../../components/AnimeCard";
 import Select from "../../components/Select";
 import { GENRES, SEASONS, SORTS, TYPES } from "../../constants";
-import latest from "../../data/latestAnime.json";
 
 const ALL = [...TYPES, ...GENRES, ...SEASONS];
 
@@ -21,33 +20,35 @@ const TypeScreen = () => {
   const current = ALL.find((type) => location.pathname.includes(type.slug));
 
   return (
-    <div className="w-full">
-      <div className="w-full p-2">
-        <div className="flex items-center justify-between">
-          <p className="text-white font-bold text-4xl">{current?.name}</p>
+    <h1>Đang làm owo</h1>
 
-          <Select
-            value={selectedSorting}
-            onChange={handleSortingSelectChange}
-            className="bg-black text-white p-2"
-          >
-            {SORTS.map((sort) => (
-              <option value={sort.slug} key={sort.slug}>
-                {sort.name}
-              </option>
-            ))}
-          </Select>
-        </div>
+    // <div className="w-full">
+    //   <div className="w-full p-2">
+    //     <div className="flex items-center justify-between">
+    //       <p className="text-white font-bold text-4xl">{current?.name}</p>
 
-        <div className="my-12 flex justify-evenly flex-wrap">
-          {latest.map((anime) => (
-            <div className="mt-2 w-44" key={anime.slug}>
-              <AnimeCard {...anime} />
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+    //       <Select
+    //         value={selectedSorting}
+    //         onChange={handleSortingSelectChange}
+    //         className="bg-black text-white p-2"
+    //       >
+    //         {SORTS.map((sort) => (
+    //           <option value={sort.slug} key={sort.slug}>
+    //             {sort.name}
+    //           </option>
+    //         ))}
+    //       </Select>
+    //     </div>
+
+    //     <div className="my-12 flex justify-evenly flex-wrap">
+    //       {latest.map((anime) => (
+    //         <div className="mt-2 w-44" key={anime.slug}>
+    //           <AnimeCard {...anime} />
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
